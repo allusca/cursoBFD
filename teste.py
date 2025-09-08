9d04b51a800cd3ea4d6510e28a5ce8ddec7b2fc3
allusca <aluscarenally7@gmail.com>
2025-09-08 17:49:09 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,86 @@
+#questao 1
+
+#numero =int(input('digite um numero: '))
+#if numero >10:
+   # print("maior que 10")
+
+#else:
+    #print ( " menor que 10")
+
+#questao 2
+
+#numero=int(input('digite um numero: '))
+    
+#if numero %2==0:
+    #print("par")
+
+#else:
+    #print("impar")
+
+#questao 3
+
+#numero=int(input('digite um numero: '))
+
+#if numero >0:
+    #print("positivo")
+    
+#elif numero <0:
+   #print("negativo")
+#else:
+     #print("zero")
+
+#questao 4
+
+#numero=int(input('digite um numero: '))
+
+#if numero ==1404:
+    #print("senha aprovada")
+
+#else:
+    #print("senha incorreta")
+
+#questao 5
+
+#numero=int(input('digite um numero: '))
+
+#if numero >18:
+    #print("maior de idade")
+    
+#else:
+    #print("menor de idade")
+
+#questao 6
+
+#numero=int(input('digite um numero: '))
+#a=int(input('digite um numero: '))
+#b=int(input('digite outro numero: '))
+
+#if a >= b
+    #print("a")
+
+#else a < b
+    #print("b")
+
+#questao 7
+
+#a=int(input('digite o primeiro numero: '))
+#b=int(input('digite o segundo numero: '))
+#c=int(input('digite o terceiro numero: '))
+
+#if a >= b and a >= c:
+    #print(a)
+
+#elif b >= a and b >= c:
+    #print(b)
+#else:
+    #print(c)
+
+nota=float(input('digite a nota: '))
+if nota >=7:
+    print("aprovado")
+    elif nota
+      
+
+
+if a + b + c: 
+    print("resultado")
